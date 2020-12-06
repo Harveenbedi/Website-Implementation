c84ea3cafd3824b8457aa731efca670f147feb1b
Harveenbedi <74007356+Harveenbedi@users.noreply.github.com>
2020-12-06 00:52:46 +0000

    Add files via upload

---
+++ react-navbar-tutorial-home/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Videos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Articles and Documentaries',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Quizzes',
+        url: '#',
+        cName: 'nav-links'
+    }
+]

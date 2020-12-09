12bdffacdb49a381eb459e433c3719653f6271c0
BeckoUK <veselin91georgiev@gmail.com>
2020-12-09 01:12:06 +0000

    RickRolled
    lol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css';
+import ReactPlayer from "react-player"
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <ReactPlayer
+     url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+     />
+    <h1>
+    You have been Rick rolled
+    </h1>
     </div>
+
   );
 }
 
